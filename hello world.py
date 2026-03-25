9779aad367035656730cac66cb4301d8ccf14955
Isaac D2 <hi@isaacd2.com>
2026-03-25 18:49:20 -0400

    removed non-used files

--- hello world.py
+++
@@ -1,7 +0,0 @@
-print("Hello, world!")
-
-class_name = "CSC 820: Large-Scale Software Systems"
-professor_name = "Russell Martin"
-semester = "Spring 2026"
-
-print(f"This class is titled \"{class_name}\" taught by {professor_name} during the {semester} semester.")
