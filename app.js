23d312001fae457b569b4e9ba9868e81e8010c2e
Isaac D2 <hi@isaacd2.com>
2026-04-05 14:20:57 -0400

    fixed cached versions

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ app.use(express.json());
 app.get('/', (req, res) => {
     res.json({
         status: 'Running',
-        version: 'v1.0.0',
+        version: 'v1.0.1',
         timestamp: new Date().toISOString()
     });
 });
